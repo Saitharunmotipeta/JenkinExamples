573965b25967d5654fa3fad2a243299e87eeb6d3
Saitharunmotipeta <156516213+Saitharunmotipeta@users.noreply.github.com>
2025-03-29 13:06:17 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,11 @@
+import sys
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+r1=n1+n2
+r2=n1-n2
+r3=n1*n2
+r4=n1/n2
+print('sum is ',r1)
+print('diff is ',r2)
+print('product is ',r3)
+print('div is ',r4)
