fddca23c73819013767f7fac164fe7463c4718a9
Saitharunmotipeta <156516213+Saitharunmotipeta@users.noreply.github.com>
2025-03-29 12:42:54 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,14 @@
+class CommandLineDemo2
+{
+public static void main(String args[])
+{
+String s1 = args[0];
+String s2 = args[1];
+String s3 = args[2];
+System.out.println("Name is "+s1);
+System.out.println("Father name is "+s2);
+System.out.println("Mother name is "+s3);
+}
+}
+
+
